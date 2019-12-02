f7dd8815811ab63ec42eeef9459eb3e10bb5e952
root <root@localhost.localdomain>
2019-12-02 17:07:43 +0530

    first commit

---
+++ CSS/Js/first.js
@@ -0,0 +1,13 @@
+let a=10;
+console.log(a);
+function num()
+{
+    return 17;
+}
+const acd=num();
+console.log(acd);
+const bc=100;
+const ad ="my name is vaibhav singh rawat"
+console.log(bc);
+console.log(ad);
+document.getElementById("test");
