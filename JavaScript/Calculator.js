e9624f2669f4300ef1dc4cc693e78447ea17fdb4
root <root@localhost.localdomain>
2019-12-03 11:06:08 +0530

    cal

---
+++ JavaScript/Calculator.js
@@ -0,0 +1,90 @@
+let tempNumber=0;
+function insert(num)
+{
+    if(tempNumber!=0)
+    {
+    document.getElementById('output').value+=num;
+    tempNumber+=num;
+    }
+    else
+    {
+        document.getElementById('output').value=num;
+        tempNumber=num;
+    }   
+}
+let number1, number2,choice;
+function firstValue()
+{
+    number1=document.getElementById('output').value;
+    document.getElementById('output').value=0;
+    tempNumber=0;
+}
+function clearEverything()
+{
+    tempNumber=0;
+    document.getElementById('output').value=0;
+}
+function solver()
+{
+    switch(choice)
+    {
+        case "additionChoice":
+            {
+                number2=document.getElementById('output').value;
+                document.getElementById('output').value=parseInt(number1)+parseInt(number2);
+                tempNumber=0;
+                 break;
+            }
+        case "substractionChoice":
+            {
+                number2=document.getElementById('output').value;
+                document.getElementById('output').value=parseInt(number1)-parseInt(number2);
+                tempNumber=0;
+                 break;
+
+            }
+        case "divisionChoice":
+            {
+                number2=document.getElementById('output').value;
+                document.getElementById('output').value=parseInt(number1)/parseInt(number2);
+                tempNumber=0;
+                 break;
+            }
+        case "multiplicationChoice":
+            {
+                number2=document.getElementById('output').value;
+                document.getElementById('output').value=parseInt(number1)*parseInt(number2);
+                tempNumber=0;
+                 break;
+            }
+    }
+}
+function remove()
+{       
+let string = document.getElementById('output').value;
+tempNumber=string.substring(0,string.length-1);
+document.getElementById('output').value=tempNumber;
+}
+
+
+function addition()
+{
+firstValue();
+choice="additionChoice";
+}
+function substraction()
+{
+   firstValue();
+   choice="substractionChoice";
+}
+
+function multiplication()
+{
+   firstValue();
+   choice="multiplicationChoice"
+}
+function division()
+{
+    firstValue();
+    choice ="divisionChoice";
+}
